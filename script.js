51f4841d2a42dc81294cd4bb1d01cf6fce84ab3a
Risi2004 <risikesanjegatheesan@gmail.com>
2025-03-22 06:26:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var signinbtn = document.getElementById("signinbtn");
+var signupbtn = document.getElementById("signupbtn");
+
+signinbtn.addEventListener("click",function(){
+    window.location.href = "signin.html"
+})
+
+signupbtn.addEventListener("click",function(){
+    window.location.href = "signup.html"
+})
